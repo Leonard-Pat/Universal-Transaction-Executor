e0c831199d18bf9a4f3dc238d9777f5f4586d847
Leonard Paturel <leonardpaturel@gmail.com>
2023-11-20 12:33:24 +0000

    centered div

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 		<html lang="en">
 			<body className={inter.className}>
 				<Banner></Banner>
-				<div className="absolute m-0 min-h-[calc(100%-87px)] min-w-full bg-black">{children}</div>
+				<div className="absolute m-0 min-h-[calc(100%-87px)] min-w-full bg-black flex items-center justify-center">{children}</div>
 			</body>
 		</html>
 	);
