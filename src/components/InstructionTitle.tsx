29e246e662f91fc94a590bc58b214fd6b95e9105
Leonard Paturel <leonardpaturel@gmail.com>
2023-11-21 09:45:41 +0000

    better colours

--- src/components/InstructionTitle.tsx
+++ src/components/InstructionTitle.tsx
@@ -4,7 +4,11 @@ function InstructionTitle() {
 	return (
 		<div className="mb-5">
 			<h3 className="self-center text-center font-turret-road text-2xl font-light text-white">
-				Enter calldata as a JSON array
+				Enter calldata as a {' '}
+				<span className="bg-gradient-to-br from-pink-500 to-orange-400 bg-clip-text text-transparent">
+					 JSON
+				</span>{' '}
+				array
 			</h3>
 		</div>
 	);
