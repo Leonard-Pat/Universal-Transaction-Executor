b5453f6811628f3054267499ab310043088f1fc5
Leonard Paturel <leonardpaturel@gmail.com>
2023-11-22 10:05:30 +0000

    added alert

--- src/components/InstructionTitle.tsx
+++ src/components/InstructionTitle.tsx
@@ -6,7 +6,7 @@ function InstructionTitle() {
 			<h3 className="self-center text-center font-turret-road text-2xl font-light text-white">
 				Enter data as a{' '}
 				<span className="bg-gradient-to-br from-pink-500 to-orange-400 bg-clip-text text-transparent">
-					JSON
+					DEVELOP
 				</span>{' '}
 			</h3>
 		</div>
